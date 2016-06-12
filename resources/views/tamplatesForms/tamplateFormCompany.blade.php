ef7e7fe3e156f26fa9299aa9ead0fd314b668d75
ewerton <ewerton@exeplo.com>
2016-06-12 15:53:42 -0300

    Atualização - Cadastro Empresa

--- resources/views/tamplatesForms/tamplateFormCompany.blade.php
+++ resources/views/tamplatesForms/tamplateFormCompany.blade.php
@@ -39,19 +39,6 @@
                 </div>
             </div>
 		</div>
-            
-             <div class="row">
-
-            <div class="col-md-3">
-                <div class="btn-group btn-group-justified">
-                    <div class="btn-group">
-                        <a href="" class="btn btn-primary btn-block"><i
-                                    class="fa fa-long-arrow-left"></i> Voltar</a></div>
-                    <div class="btn-group">
-                        {!! Form::submit('Salvar', array('class' => 'btn btn-primary btn-block')) !!}
-                    </div>
-                </div>
-            </div>
         </div>  
 	</div>
 </div>
