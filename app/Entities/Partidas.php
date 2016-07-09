8225beda774148cc57949a1fff45d44381ee8059
andreyserbinario <andreyfbs2013@gmail.com>
2016-07-09 19:31:17 -0300

    Atualizações com a remoção do provider seracademico

--- app/Entities/Partidas.php
+++ app/Entities/Partidas.php
@@ -6,14 +6,77 @@ use Illuminate\Database\Eloquent\Model;
 use Prettus\Repository\Contracts\Transformable;
 use Prettus\Repository\Traits\TransformableTrait;
 
-class Partida extends Model implements Transformable
+class Partidas extends Model implements Transformable
 {
     use TransformableTrait;
 
+    /**
+     * @var string
+     *
+     * Nome da tabela do banco de dados
+     */
     protected $table    = "partidas";
 
-    protected $fillable = [
-        
+    /**
+     * @var array
+     *
+     * Campos do tipo data
+     */
+    protected $dates    = [
+        'data'
     ];
 
+    /**
+     * @var array
+     *
+     * MassAssigment
+     * Campos para cadastro automático
+     */
+    protected $fillable = [
+        'data',
+        'time_casa_id',
+        'tima_fora_id',
+        'status_id',
+        'campeonato_id'
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     *
+     * Relacionamento com Times (da casa)
+     */
+    public function timeCasa()
+    {
+        return $this->belongsTo(Times::class, 'tima_casa_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     *
+     *  Relacionamento com Times (de fora)
+     */
+    public function timeFora()
+    {
+        return $this->belongsTo(Times::class, 'tima_fora_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     *
+     *  Relacionamento com Status
+     */
+    public function status()
+    {
+        return $this->belongsTo(Status::class, 'status_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     *
+     *  Relacionamento com Camponatos
+     */
+    public function campeonato()
+    {
+        return $this->belongsTo(Campeonatos::class, 'campeonato_id');
+    }
 }
--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -23,6 +23,8 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->bind(\Softage\Repositories\UserRepository::class, \Softage\Repositories\UserRepositoryEloquent::class );
+        $this->app->bind(\Softage\Repositories\RoleRepository::class, \Softage\Repositories\RoleRepositoryEloquent::class);
         $this->app->bind(\Softage\Repositories\AreasRepository::class, \Softage\Repositories\AreasRepositoryEloquent::class);
         $this->app->bind(\Softage\Repositories\ParametrosRepository::class, \Softage\Repositories\ParametrosRepositoryEloquent::class);
         $this->app->bind(\Softage\Repositories\CampeonatosRepository::class, \Softage\Repositories\CampeonatosRepositoryEloquent::class);
@@ -36,7 +38,7 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->bind(\Softage\Repositories\PremiacoesRepository::class, \Softage\Repositories\PremiacoesRepositoryEloquent::class);
         $this->app->bind(\Softage\Repositories\VendedorRepository::class, \Softage\Repositories\VendedorRepositoryEloquent::class);
         $this->app->bind(\Softage\Repositories\ConfVendasRepository::class, \Softage\Repositories\ConfVendasRepositoryEloquent::class);
-        $this->app->bind(\App\Repositories\PartidaRepository::class, \App\Repositories\PartidaRepositoryEloquent::class);
+        $this->app->bind(\Softage\Repositories\PartidaRepository::class, \App\Repositories\PartidaRepositoryEloquent::class);
         //:end-bindings:
     }
 }
