b4afbf7fdaec632232a8d9fe05f2a1ad949ed52d
Fabio Aguiar <fabinhobarreto2@gmail.com>
2016-10-01 16:29:19 -0300

    criando seed de users

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \Softage\Entities\User::truncate();
+
+        \Softage\Entities\User::create([
+            'name' => 'Andrey Fernandes',
+            'email' => 'andrey@email.com',
+            'password' => bcrypt(123456),
+            'remember_token' => str_random(10)
+        ]);
+        
+    }
+}
